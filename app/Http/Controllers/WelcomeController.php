8c61d5997d4df5786c13c4038c23f48008f5ad48
Fanani <azfann.natinons@gmail.com>
2026-03-01 21:53:43 +0700

    mplementasi View, Sub-folder, dan Passing Data

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class WelcomeController extends Controller
 {
     //langkah 2
-    public function hello() {
-        return 'Hello World';
-    }
+    public function hello(){ 
+        return('Hello World'); 
+    } 
+    public function greeting(){ 
+        return view('blog.hello')
+            ->with('name','Fanani')
+            ->with('occupation','Astronaut');
+    } 
 }
